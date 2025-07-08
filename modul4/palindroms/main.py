1d98d283d1b8b259d36a0929a0337b8f9172962b
Paweł Kępczyński <pawel.k1997@gmail.com>
2025-07-08 18:07:04 +0200

    Add documentation for 'palindroms' function

--- modul4/palindroms/main.py
+++ modul4/palindroms/main.py
@@ -1,4 +1,14 @@
 def palindroms(word):
+    """
+    Checks if the given word is a palindrome.
+
+    Arguments:
+    word: The string to be checked.
+
+    Returns:
+    True: If the word is a palindrome (reads the same forwards and backward).
+    False: If the word is not a palindrome.
+    """
     for i in range(len(word) // 2):
         if word[i] != word[len(word) - 1 - i]:
             return False
